a4887026b82f1c245b25fd0c3492c45ba65cee9d
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-07-29 22:34:32 +0700

    #2 Basic UI - Get Started

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -11,13 +11,13 @@ import NavigationPane from "src/components/navigation_pane";
 export default function DashboardLayout() {
   return (
     <>
-      <section className="float-left w-[25%] h-screen bg-background-10">
+      <section className="float-left w-[25%] h-screen bg-background-10 max-[1140px]:hidden">
         <header className="m-6 p-6 outline outline-1 outline-on-background-10 rounded-xl bg-background">
           <h1 className="font-semibold text-xl">Aptopus</h1>
         </header>
         <NavigationPane />
       </section>
-      <section className="float-right w-[75%] h-screen py-6 bg-background">
+      <section className="float-right w-[75%] h-screen py-6 bg-background max-[1140px]:w-full">
         <Outlet />
       </section>
     </>
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+// import React from "react";
+
+// Import from local
+// Import components
+import AnwserBox from "./components/AnwserBox";
+import AskBox from "./components/AskBox";
+import MessageScrollBox from "./components/MessageScrollBox";
+import QuestionBox from "./components/QuestionBox";
 
 export default function ChatPage() {
   return (
-    <div className="w-full h-full p-6 outline outline-1 outline-on-background-10 rounded-bl-xl rounded-tl-xl bg-background">
-      Chat Section
+    <div className="flex w-full h-full outline outline-1 outline-on-background-10 rounded-bl-xl rounded-tl-xl bg-background">
+      {/* Chat Section */}
+      <section className="w-[65%] min-w-[620px] max-[1280px]:w-full">
+        <MessageScrollBox
+          renderAnswer={AnwserBox}
+          renderQuestion={QuestionBox}
+        />
+        <AskBox />
+      </section>
+
+      {/* Another Section, that use to build some future feature: History of Chat, Organize Chats */}
+      <div className="w-[35%] outline outline-1 outline-on-background-10 max-[1280px]:hidden"></div>
     </div>
   );
 }
